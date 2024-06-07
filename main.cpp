319bbd54bc46c264c5e4739031ebca378aa5d46f
junhoyun <sos84111@snu.ac.kr>
2024-06-07 08:30:08 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -7,7 +7,21 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-	cout<<cnt;
+	if(cnt<M){
+		for(int i=1;i<N-M+2+cnt;i++){
+			if(cnt==0||i>vec[cnt-1]){
+				vec.push_back(i);
+				func(cnt+1);
+				vec.pop();
+			}	
+		}
+	}
+	else{
+		for(int i=0;i<M;i++){
+			cout<<vec[i]<<' ';
+		}
+		cout<<endl;
+	}
 
 
 
@@ -17,6 +31,7 @@ void func(int cnt) {
 }
 
 int main() {
-   cin >> N >> M;
+   cin >> N >> M
+	   ;
    func(0);
 }
